8c8c7876ddabc3ddb926a05a06f6ee1aacc5bd9b
Statkeivch Roman <romanstat@mail.ru>
2021-03-30 09:59:09 +0300

    Added CarMileageEqualityComparer

---
+++ Practise/CarMileageEqualityComparer.cs
@@ -0,0 +1,19 @@
+﻿using Practise.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Practise
+{
+    class CarMileageEqualityComparer : IEqualityComparer<Car>
+    {
+        public bool Equals(Car x, Car y)
+        {
+            return x.Mileage == y.Mileage;
+        }
+
+        public int GetHashCode([DisallowNull] Car obj)
+        {
+            return obj.Mileage.GetHashCode();
+        }
+    }
+}
