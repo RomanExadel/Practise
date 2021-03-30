7f8d5c37b7e77e96608cf18449fe90d6887de162
Statkeivch Roman <romanstat@mail.ru>
2021-03-30 13:35:54 +0300

    Implemented SRP

---
+++ SOLID/SRP/MailSender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SOLID
+{
+    class MailSender
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public void SendEmail()
+        {
+            Console.WriteLine("Email send!");
+        }
+    }
+}
