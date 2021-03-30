947695d5bb2e1aeebcebc7f59c5cadf40152cb40
Statkeivch Roman <romanstat@mail.ru>
2021-03-30 10:16:09 +0300

    Implemented ProcessNameEqualityComparer

---
+++ Practise/ProcessNameEqualityComparer.cs
@@ -0,0 +1,20 @@
+﻿using Practise.Structures;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Practise
+{
+    struct ProcessNameEqualityComparer : IEqualityComparer<Process>
+    {
+        public bool Equals(Process process1, Process process2)
+        {
+            return string.Equals(process1.Name, process2.Name);
+        }
+
+        public int GetHashCode([DisallowNull] Process obj)
+        {
+            return HashCode.Combine(obj.Name);
+        }
+    }
+}
