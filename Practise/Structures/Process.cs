45a3be7692e4dbd5652ff534c942e67d89047042
Statkeivch Roman <romanstat@mail.ru>
2021-03-30 10:18:23 +0300

    Implemented Process structure

---
+++ Practise/Structures/Process.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Practise.Structures
+{
+    struct Process : IProcess
+    {
+        private string _name;
+        
+        public Process(string name)
+        {
+            this = new Process();
+            Name = name;
+        }
+
+        public int RunningTime { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                _name = value;
+            }
+        }
+
+        public static void ChangeRunningTime(IProcess process)
+        {
+            process.RunningTime = 100;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Process started");
+        }
+
+        public static Process operator ++(Process process)
+        {
+            process.RunningTime++;
+            return process;
+        }
+
+        public static Process operator --(Process process)
+        {
+            process.RunningTime--;
+            return process;
+        }
+    }
+}
