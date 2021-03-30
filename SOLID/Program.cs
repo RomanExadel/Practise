8d23ab6783046960d6c9e5166aa327fcde634474
Statkeivch Roman <romanstat@mail.ru>
2021-03-30 18:09:14 +0300

    Added UnityContainer

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace SOLID
+﻿using SOLID.DIP;
+using System;
+using Unity;
+
+namespace SOLID
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<IEmployeeDataAccess, EmployeeDataAccess>();
 
+            EmployeeBusinessLogic employee = container.Resolve<EmployeeBusinessLogic>();
+            Console.WriteLine(employee.GetEmployeeDetails(1));
         }
     }
 }
