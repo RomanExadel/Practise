9ffd2f72195764c14e6f7e9e9276a281e60d740c
Statkeivch Roman <romanstat@mail.ru>
2021-03-31 12:32:10 +0300

    Added UnitTest for EmployeeBL

---
+++ UnitTests/MockData/EmployeeMockData.cs
@@ -0,0 +1,20 @@
+﻿using SOLID.DIP;
+
+namespace UnitTests.DbContext
+{
+    public static class EmployeeMockData
+    {
+        public static Employee GetEmployeeDetails(int id)
+        {
+            Employee emp = new()
+            {
+                Id = id,
+                Name = "Your",
+                Department = "IT",
+                Salary = 10000
+            };
+
+            return emp;
+        }
+    }
+}
