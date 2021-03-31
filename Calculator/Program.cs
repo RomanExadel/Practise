4a5eeaace7f4b5f054f611f12a5a0e25f3dc7da3
Statkeivch Roman <romanstat@mail.ru>
2021-03-31 18:56:07 +0300

    Impelemnted SimpleAlgorithrm

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,22 +9,15 @@ namespace Calculator
         {
             try
             {
-                Console.WriteLine("Write instruction in form <A> <operation> <B> =");
+                Console.WriteLine("Write instruction in form <A> <operation> <B> <operation> <C> ... =");
                 var instruction = Console.ReadLine();
                 Console.SetCursorPosition(instruction.Length + 1, 1);
 
-                if (Regex.IsMatch(instruction, @"^\-?\d{1,17}\s+[\+\-*/]\s+\-?\d{1,17}\s+=$"))
+                if (Regex.IsMatch(instruction, @"^-?\d{1,17}(\s*[-+ */]\s*-?\d{1,17})*\s*=$"))
                 {
-                    var splitedExpression = instruction.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                    var @operator = char.Parse(splitedExpression[1]);
-                    var leftOperand = long.Parse(splitedExpression[0]);
-                    var rightOperand = long.Parse(splitedExpression[2]);
-
-                    var user = new User();
-                    user.Compute('+', leftOperand);
-                    user.Compute(@operator, rightOperand);
-
-                    Console.Write(user.CurrentValue);
+                    var simpleAlgorithm = new SimpleAlgorithm();
+                    simpleAlgorithm.Algorithm(instruction);
+                    Console.Write(simpleAlgorithm.Result);
                 }
                 else
                 {
---
+++ Calculator/SimpleAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    class SimpleAlgorithm
+    {
+        private readonly string splitPattern = "(?<=[()-+*/])|(?=[()-+*/])|([=])";
+        private readonly int numberOfExtraSigns = 1;
+        private User user = new User();
+
+        public long Result
+        {
+            get => user.CurrentValue;
+        }
+
+        public void Algorithm(string instruction)
+        {
+            var splitedExpression = Regex.Split(instruction, splitPattern).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            var firstNumber = long.Parse(splitedExpression[0]);
+            user.Compute('+', firstNumber);
+
+            for (int i = 1; i < splitedExpression.Length - numberOfExtraSigns; i += 2)
+            {
+                var @operator = char.Parse(splitedExpression[i]);
+                var operand = long.Parse(splitedExpression[i + 1]);
+                user.Compute(@operator, operand);
+            }
+        }
+    }
+}
