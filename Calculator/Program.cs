bdb725f81b805ce2c11a2a907da7129a74009b6c
Statkeivch Roman <romanstat@mail.ru>
2021-03-31 19:44:57 +0300

    Added endless check

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,17 +7,21 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int numberOfInstruction = 1;
+
+            label1:
+
             try
             {
                 Console.WriteLine("Write instruction in form <A> <operation> <B> <operation> <C> ... =");
                 var instruction = Console.ReadLine();
-                Console.SetCursorPosition(instruction.Length + 1, 1);
+                Console.SetCursorPosition(instruction.Length + 1, numberOfInstruction);
 
                 if (Regex.IsMatch(instruction, @"^-?\d{1,17}(\s*[-+ */]\s*-?\d{1,17})*\s*=$"))
                 {
                     var simpleAlgorithm = new SimpleAlgorithm();
                     simpleAlgorithm.Algorithm(instruction);
-                    Console.Write(simpleAlgorithm.Result);
+                    Console.WriteLine(simpleAlgorithm.Result);
                 }
                 else
                 {
@@ -28,6 +32,9 @@ namespace Calculator
             {
                 Console.WriteLine(ex.Message);
             }
+
+            numberOfInstruction += 2;
+            goto label1;
         }
     }
 }
