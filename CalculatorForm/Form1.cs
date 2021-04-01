d9c66c6413c26e13ae9097bf47290a6f45bcbf74
Statkeivch Roman <romanstat@mail.ru>
2021-04-01 20:27:36 +0300

    Added CalculatorFrom project

---
+++ CalculatorForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Calculator;
+using System;
+using System.Windows.Forms;
+
+namespace CalculatorForm
+{
+    public partial class Form1 : Form
+    {
+        private readonly Context _context;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _context = new Context(new ReversePolishNotationAlgorithm());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var answer = _context.ExecuteAlgorithm(textBox1.Text);
+                label3.Text = $"Answer: {answer}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
